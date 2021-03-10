f2743147536466e6c63d8e38cb09ca79b726e02d
th3vk <thevklol@gmail.com>
2021-03-10 17:39:31 +0500

    Mistral Mafia is now Mistral Trading Company Updated some animations, made initial tilesets (they'll require adjustments) Made a party select menu Added faction logos Added the first map and gameplay collection

---
+++ main/mainmenu_gui.go
@@ -0,0 +1,15 @@
+components {
+  id: "Mainmenu"
+  component: "/main/Mainmenu.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
