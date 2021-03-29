eeee8ec344111006809bdcdcb2553fbff7211274
th3vk <thevklol@gmail.com>
2021-03-30 03:01:37 +0500

    Added basic squad-pick GUI

---
+++ main/pick_squad_gui.go
@@ -0,0 +1,15 @@
+components {
+  id: "pick_squad_gui"
+  component: "/main/pick_squad_gui.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
